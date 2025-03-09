22c21f27a9a178352de093de1adb9ace1c5ff502
takina <3362747864@qq.com>
2025-03-09 16:24:47 +0800

    添加springboot模板

--- yuzi-generator-basic/src/main/java/com/yupi/generator/MainGenerator.java
+++ yuzi-generator-basic/src/main/java/com/yupi/generator/MainGenerator.java
@@ -1,5 +1,6 @@
-package com.yupi.maker.generator.file;
+package com.yupi.generator;
 
+import com.yupi.model.MainTemplateConfig;
 import freemarker.template.TemplateException;
 
 import java.io.File;
@@ -8,7 +9,7 @@ import java.io.IOException;
 /**
  * 核心生成器
  */
-public class FileGenerator {
+public class MainGenerator {
 
     /**
      * 生成
@@ -25,10 +26,18 @@ public class FileGenerator {
         String inputPath = new File(parentFile, "yuzi-generator-demo-projects/acm-template").getAbsolutePath();
         String outputPath = projectPath;
         // 生成静态文件
-        StaticFileGenerator.copyFilesByHutool(inputPath, outputPath);
+        StaticGenerator.copyFilesByRecursive(inputPath, outputPath);
         // 生成动态文件
-        String inputDynamicFilePath = projectPath + File.separator + "src/main/resources/templates/DataModel.java.ftl.ftl";
+        String inputDynamicFilePath = projectPath + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
         String outputDynamicFilePath = outputPath + File.separator + "acm-template/src/com/yupi/acm/MainTemplate.java";
-        DynamicFileGenerator.doGenerate(inputDynamicFilePath, outputDynamicFilePath, model);
+        DynamicGenerator.doGenerate(inputDynamicFilePath, outputDynamicFilePath, model);
+    }
+
+    public static void main(String[] args) throws TemplateException, IOException {
+        MainTemplateConfig mainTemplateConfig = new MainTemplateConfig();
+        mainTemplateConfig.setAuthor("yupi");
+        mainTemplateConfig.setLoop(false);
+        mainTemplateConfig.setOutputText("求和结果：");
+        doGenerate(mainTemplateConfig);
     }
 }
--- yuzi-generator-maker/src/main/java/com/yupi/maker/meta/MetaManager.java
+++ yuzi-generator-maker/src/main/java/com/yupi/maker/meta/MetaManager.java
@@ -6,20 +6,14 @@ import cn.hutool.json.JSONUtil;
 public class MetaManager {
 
     private static volatile Meta meta;
-//volatile关键字的作用当多个线程同时访问同一个 volatile
-// 变量时，每个线程对这个变量的读取操作都会直接从主内存中读取最新的值，
-// 而不是从线程本地缓存中读取。
+
     private MetaManager() {
         // 私有构造函数，防止外部实例化
     }
-//单例双检索锁设计模式，这里是判断两次null
+
     public static Meta getMetaObject() {
-//        这个null判断能够在初始化后的情况下，减少不必要的开销
         if (meta == null) {
-//            加锁防止多个 thread 同时初始化 meta 对象
             synchronized (MetaManager.class) {
-//                判断meta是否为空，为空才进行初始化，a抢到锁
-//                初始化后，bc再次进行初始化防止多次初始化
                 if (meta == null) {
                     meta = initMeta();
                 }
@@ -29,10 +23,11 @@ public class MetaManager {
     }
 
     private static Meta initMeta() {
-        String metaJson = ResourceUtil.readUtf8Str("meta.json");
+//        String metaJson = ResourceUtil.readUtf8Str("meta.json");
+        String metaJson = ResourceUtil.readUtf8Str("springboot-init-meta.json");
         Meta newMeta = JSONUtil.toBean(metaJson, Meta.class);
-        Meta.FileConfig fileConfig = newMeta.getFileConfig();
-        // todo 校验和处理默认值
+        // 校验和处理默认值
+        MetaValidator.doValidAndFill(newMeta);
         return newMeta;
     }
 }
